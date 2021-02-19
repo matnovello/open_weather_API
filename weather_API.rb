002042a5038c3c22e09e7b48b004d0b4c2a62416
Mat <mvello66@gmail.com>
2021-02-18 21:00:35 -0500

    add quit functionatlity to weather app

--- weather_API.rb
+++ weather_API.rb
@@ -18,6 +18,12 @@ description = response.parse["weather"][0]["description"]
 while true
   puts "enter a city name for currentv weather conditions"
   input_city = gets.chomp
+
+  if input_city == "q"
+    puts "OK, you quit"
+    break
+  end
+
   response = HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=#{input_city}&appid=#{ENV["OPEN_WEATHER_API_KEY"]}&units=imperial")
 
   puts "It's Currently #{temp} in #{input_city} with #{description}"
