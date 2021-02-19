ce00c334c2edec0b803c5332245d9ae84d1641b3
Mat <mvello66@gmail.com>
2021-02-18 20:55:42 -0500

    dynamic weather API with user input

---
+++ weather_API.rb
@@ -0,0 +1,24 @@
+require "http"
+
+response = HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=brooklyn&appid=#{ENV["OPEN_WEATHER_API_KEY"]}&units=imperial")
+
+#temperature min/max, description, wind speed, etc.
+#temp
+temp = response.parse["main"]["temp"]
+#max temp
+temp_min = response.parse["main"]["temp_min"]
+#min temp
+temp_max = response.parse["main"]["temp_max"]
+#feels like
+temp_feels = response.parse["main"]["feels_like"]
+#description
+description = response.parse["weather"][0]["description"]
+#wind speed
+
+while true
+  puts "enter a city name for currentv weather conditions"
+  input_city = gets.chomp
+  response = HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=#{input_city}&appid=#{ENV["OPEN_WEATHER_API_KEY"]}&units=imperial")
+
+  puts "It's Currently #{temp} in #{input_city} with #{description}"
+end
